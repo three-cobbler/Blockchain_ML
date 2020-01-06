4df51aeee49d97005c9785f51d8b939c254012a0
Guitarja <124254388@qq.com>
2020-01-06 02:08:34 -0800

    update block example

---
+++ block.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+block = {
+    "nonHashData": {
+        "index": 1
+        "localCommitTimestamp": {
+            "nanos": 975295157,
+            "seconds": 1466057539 #Unix style time format
+        },
+        "transactionResults": 
+            {
+                "uuid": "hashed uuid
+            }
+        
+    'transactions': 
+        {
+            'committer': "hashed committer name"
+            'git diff": {
+            'comments': {
+                        "957fbc92b123030c389bf8b4b874522bdf2db72c add feature
+                        ce489262a1ee34340440e55a0b99ea6918e19e7a rename some classes
+                        6b539f280d8b0ec4874671bae9c6bed80b788006 refactor some code
+                        646e7863348a427e1ed9163a9a96fa759112f102 add some copy to body"
+                        }#comments
+            'code': {
+                        @@ -34,6 +34,8 @@
+                        -this is a git diff test example
+                        +this is a diff example
+                    } # code change
+            'metrics': "confusion matrix"
+            'type': "update"
+        }
+    }
+    
+    'previous_hash': "2cf24dba5fb120a30e26e183b2ac5b9e294e1b161e5c1fa72425e735043362938b92824"
+    }
+    
